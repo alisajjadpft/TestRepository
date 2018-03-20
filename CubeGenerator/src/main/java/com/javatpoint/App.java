c01ef65af7ebb80604ef3e60f346b1349ef59070
Syed Ali Sajjad Raza <syed.raza@primefocus.com>
2018-03-20 15:13:19 +0530

    Adding code

---
+++ CubeGenerator/src/main/java/com/javatpoint/App.java
@@ -0,0 +1,16 @@
+package com.javatpoint;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+//		int junk =0;
+		int best =0;  
+//		int test =0; 
+	}
+}
